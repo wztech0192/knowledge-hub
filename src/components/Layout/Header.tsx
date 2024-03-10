5744a56e440624b0700b24341332a8158c4367f0
Soumik Mitra <soumikdeb2@gmail.com>
2024-03-10 15:07:12 -0400

    Added a search box in the header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,13 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Box, IconButton, Stack, styled } from '@mui/material';
+// import Typography from '@mui/material/Typography';
+import {alpha} from '@mui/material/styles'
+import { IconButton, Stack, styled } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
 import { ThemeToggle } from './ThemeToggle';
 import { NavLink } from 'react-router-dom';
 import { Home } from '@mui/icons-material';
-import logo from '@/assets/react.svg';
+import SearchIcon from '@mui/icons-material/Search';
 
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
   background: theme.palette.primary.main,
@@ -15,22 +17,64 @@ const StyledAppBar = styled(AppBar)(({ theme }) => ({
   borderBottomRightRadius: 16,
 }));
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
 export const Header = () => {
   return (
-    <StyledAppBar position="sticky">
+    <StyledAppBar position="static">
       <Toolbar
         sx={{ minHeight: '56px !important', justifyContent: 'space-between' }}>
         <IconButton color="inherit" component={NavLink} to="/">
           <Home />
         </IconButton>
         <Stack direction="row" alignItems="center">
-          <Typography
-            fontSize={{ xs: '1.5rem', md: '2rem' }}
-            color="inherit"
-            fontWeight={500}>
-            Knowledge Hub
-          </Typography>
-          <Box component="img" width="50px" src={logo} />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
         </Stack>
 
         <ThemeToggle />
