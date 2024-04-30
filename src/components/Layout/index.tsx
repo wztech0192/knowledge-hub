fc807255a1e59895b685831fd9454f41e72ea246
Soumik Mitra <soumikdeb2@gmail.com>
2024-04-30 00:07:32 -0400

    reverted back to the background animation where the website used to have a wave animation. #2

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,9 +6,10 @@ import '../../assets/css/index.scss';
 export const Layout = () => {
   return (
     <Box className="container">
-      <div className="background-animation">
-   ...
-  </div>
+      <div className="ocean">
+        <div className="wave"></div>
+        <div className="wave"></div>
+      </div>
       <Header />
       <br />
       <Container maxWidth="md" style={{ position: 'relative', zIndex: 1 }}>
